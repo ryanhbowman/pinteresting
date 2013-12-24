abd5206a9426dac820c1b60b09bff673a48df452
Ryan Bowman <ryan@circle.co.ke>
2013-12-24 17:53:05 +0000

    bunch of changes, including pins and styles thereof

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  # from scaffold pins, creates /pins and pins/new
+  resources :pins
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
